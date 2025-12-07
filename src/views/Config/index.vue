e6f1cf45b49064a0182686caab2e2efce4393fd7
Mingjie He <mingjiehe@Mingjies-MacBook-Air.local>
2025-12-07 20:15:25 +0800

    remove: 移除配置页面底部的版权信息和社交媒体链接

--- src/views/Config/index.vue
+++ src/views/Config/index.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
 import { configRoutes } from '../../router'
 
-const { t } = useI18n()
 const router = useRouter()
 const route = useRoute()
 const menuList = ref<any[]>(configRoutes.children)
@@ -67,31 +65,6 @@ function skip(path: string) {
     </ul>
     <router-view class="flex-1 mt-5" />
   </div>
-  <footer class="p-10 rounded footer footer-center bg-base-200 text-base-content">
-    <nav class="grid grid-flow-col gap-4">
-      <a class="cursor-pointer link link-hover text-inherit" target="_blank" href="https://1kw20.fun">{{ t('footer.self-reflection') }}</a>
-    </nav>
-    <nav>
-      <a class="cursor-pointer link link-hover text-inherit" target="_blank" href="https://1kw20.fun">{{ t('footer.thiefEasy') }}</a>
-    </nav>
-    <nav>
-      <div class="grid grid-flow-col gap-4">
-        <a href="https://github.com/LOG1997/log-lottery" target="_blank" class="cursor-pointer text-inherit">
-          <svg-icon name="github" />
-        </a>
-        <a href="https://twitter.com/TaborSwift" target="_blank" class="cursor-pointer "><svg-icon name="twitter" /></a>
-        <a href="https://www.instagram.com/log.z1997/" target="_blank" class="cursor-pointer ">
-          <svg-icon name="instagram" />
-        </a>
-      </div>
-    </nav>
-    <aside>
-      <p class="p-0 m-0">
-        蜀ICP备2021028666号
-      </p>
-      <p>Copyright © 2024 - All right reserved by Log1997</p>
-    </aside>
-  </footer>
 </template>
 
 <style scoped></style>
